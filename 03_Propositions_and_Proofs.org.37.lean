25493f838db9151aca7e42203582196a1fa67aa5
Christopher Mazey <cjmazey@gmail.com>
2015-09-28 11:53:29 -0400

    foo

--- 03_Propositions_and_Proofs.org.37.lean
+++ 03_Propositions_and_Proofs.org.37.lean
@@ -7,13 +7,22 @@ variables p q r s : Prop
 
 -- commutativity of ∧ and ∨
 example : p ∧ q ↔ q ∧ p :=
-  iff.intro and.swap and.swap
+  have swap : ∀ {p q : Prop}, p ∧ q → q ∧ p,
+    from assume p q : Prop,
+         assume Hpq : p ∧ q,
+         show q ∧ p, from and.intro (and.right Hpq) (and.left Hpq),
+  iff.intro swap swap
 
+example : p ∧ q ↔ q ∧ p :=
+
+exit
 example : p ∨ q ↔ q ∨ p :=
   iff.intro or.swap or.swap
 
 -- associativity of ∧ and ∨
-example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := sorry
+example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) :=
+  and.assoc
+
 example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := sorry
 
 -- distributivity
@@ -33,7 +42,9 @@ example : (¬p ∨ q) → (p → q) := sorry
 example : p ∨ false ↔ p := sorry
 example : p ∧ false ↔ false := sorry
 example : ¬(p ↔ ¬p) := sorry
-example : (p → q) → (¬q → ¬p) := sorry
+
+example : (p → q) → (¬q → ¬p) :=
+  λ Hpq Hnq Hp, absurd (Hpq Hp) Hnq
 
 -- these require classical reasoning
 example : (p → r ∨ s) → ((p → r) ∨ (p → s)) := sorry
