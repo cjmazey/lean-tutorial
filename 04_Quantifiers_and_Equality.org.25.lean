847e7fd2c3bab302321d9593e4cd978da1ca57ba
Christopher Mazey <cjmazey@gmail.com>
2015-10-03 14:05:12 -0400

    exercise 4.5.4

--- 04_Quantifiers_and_Equality.org.25.lean
+++ 04_Quantifiers_and_Equality.org.25.lean
@@ -6,12 +6,15 @@ open classical
 variables (A : Type) (p q : A → Prop)
 variable r : Prop
 
+-- constructive
 example : (∃ x : A, r) → r :=
   λ H, exists.elim H (λ x Hr, Hr)
 
+-- constructive
 example (a : A) : r → (∃ x : A, r) :=
   λ Hr, exists.intro a Hr
 
+-- constructive
 example : (∃ x, p x ∧ r) ↔ (∃ x, p x) ∧ r :=
   iff.intro (assume H,
              obtain (w : A) (Hw : p w ∧ r), from H,
@@ -21,12 +24,26 @@ example : (∃ x, p x ∧ r) ↔ (∃ x, p x) ∧ r :=
              obtain (w : A) (Hw : p w), from and.left H,
              exists.intro w (and.intro Hw (and.right H)))
 
-example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) := sorry
+-- constructive
+example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) :=
+  iff.intro (assume H,
+             obtain (w : A) (Hw : p w ∨ q w), from H,
+             or.elim Hw (λ Hp, or.inl (exists.intro w Hp))
+                        (λ Hq, or.inr (exists.intro w Hq)))
+            (assume H,
+             or.elim H (assume Hp : Exists p,
+                        obtain w Hw, from Hp,
+                        exists.intro w (or.inl Hw))
+                       (assume Hq : Exists q,
+                        obtain w Hw, from Hq,
+                        exists.intro w (or.inr Hw)))
+
 
 example : (∀ x, p x) ↔ ¬ (∃ x, ¬ p x) := sorry
 example : (∃ x, p x) ↔ ¬ (∀ x, ¬ p x) := sorry
 example : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) := sorry
 example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) := sorry
+
 
 example : (∀ x, p x → r) ↔ (∃ x, p x) → r := sorry
 example (a : A) : (∃ x, p x → r) ↔ (∀ x, p x) → r := sorry
